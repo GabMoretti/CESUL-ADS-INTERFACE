564e785f9e753cfdf2c8ed7acc13937825d841b7
Gabriel Moretti Woicolesco <114514587+GabMoretti@users.noreply.github.com>
2023-06-06 21:41:34 -0300

    TarefasContext/src/App.jsx TarefasContext/src/componentes/Header.jsx TarefasContext/src/componentes/Header.module.jsx TarefasContext/src/index.css

--- TarefasContext/src/App.jsx
+++ TarefasContext/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Header from './componentes/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
---
+++ TarefasContext/src/componentes/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+
+        <header>
+
+            <nav>
+                <p>Cadastrar |  Listar</p>
+            </nav>
+
+            <section>
+                <button>Tema</button>
+            </section>
+            
+        </header>
+
+    )
+}
+
+export default Header;
