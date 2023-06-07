b8acbcedcb6663e5871770d6e702145fb3238801
Gabriel Moretti Woicolesco <114514587+GabMoretti@users.noreply.github.com>
2023-06-06 22:27:59 -0300

    modified:   TarefasContext/src/contexts/ThemeContext.jsx modified:   TarefasContext/src/index.css

--- TarefasContext/src/contexts/ThemeContext.jsx
+++ TarefasContext/src/contexts/ThemeContext.jsx
@@ -6,7 +6,7 @@ export const ThemeContext = createContext();
 //vou usar no HTML
 export const ThemeProvider = ({children}) => {
 
-    const [theme, setTheme] = useState('DARK')
+    const [theme, setTheme] = useState('LIGHT')
 
     function changeTheme(){
 
